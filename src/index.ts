5302f232848317fa2decc3a619715344b901fbdf
Aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-05-02 14:18:36 +0530

    index file, to update and log the state in every 6sec

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { games } from "./store";
+import { startLogger } from "./logger";
+
+startLogger();
+setInterval(()=>{
+    games.push({
+        id:Math.random().toString(),
+        whitePlayerName:"Alice",
+        blackPlayerName:"Bob",
+        moves:[]
+    })
+}, 6000)
